f1fa73f80cd9424e4400b3cbc427b551e14c3581
bananaback <58079308+bananaback@users.noreply.github.com>
2024-03-04 04:26:53 +0700

    Implement account registration

--- app/src/main/java/com/hcmute/endsemesterproject/MainActivity.java
+++ app/src/main/java/com/hcmute/endsemesterproject/MainActivity.java
@@ -1,14 +1,49 @@
 package com.hcmute.endsemesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private  TabAccessorAdapter mytabAccessorAdapter;
+    private FirebaseUser currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("ChatsApp");
+
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        mytabAccessorAdapter = new TabAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(mytabAccessorAdapter);
+
+        myTabLayout = (TabLayout)  findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (currentUser == null) {
+            SendUserToLoginActivity();
+        }
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
     }
 }
