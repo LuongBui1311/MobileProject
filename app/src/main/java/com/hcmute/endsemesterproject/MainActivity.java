5c87391303adc8ef669113d195b01bc73237607b
bananaback <58079308+bananaback@users.noreply.github.com>
2024-03-04 04:55:40 +0700

    Implement signout

--- app/src/main/java/com/hcmute/endsemesterproject/MainActivity.java
+++ app/src/main/java/com/hcmute/endsemesterproject/MainActivity.java
@@ -1,14 +1,20 @@
 package com.hcmute.endsemesterproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
@@ -16,10 +22,19 @@ public class MainActivity extends AppCompatActivity {
     private TabLayout myTabLayout;
     private  TabAccessorAdapter mytabAccessorAdapter;
     private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+    //private DatabaseReference RootRef;
+    private String currentUserID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        //RootRef = FirebaseDatabase.getInstance().getReference();
 
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -45,5 +60,30 @@ public class MainActivity extends AppCompatActivity {
     private void SendUserToLoginActivity() {
         Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.main_logout_option) {
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+
+        if (item.getItemId() == R.id.main_settings_option) {
+
+        }
+
+        if (item.getItemId() == R.id.main_find_friends_option) {
+
+        }
+        return true;
     }
 }
