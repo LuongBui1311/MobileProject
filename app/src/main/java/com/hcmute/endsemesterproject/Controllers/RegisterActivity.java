26583efa37b691b9f47f3aa8db06bb20b3d47e9b
lamtanng <laam122003@gmail.com>
2024-05-05 18:23:56 +0700

    feat/MCHAT-18: Validate input email +password (register)

--- app/src/main/java/com/hcmute/endsemesterproject/Controllers/RegisterActivity.java
+++ app/src/main/java/com/hcmute/endsemesterproject/Controllers/RegisterActivity.java
@@ -1,8 +1,5 @@
 package com.hcmute.endsemesterproject.Controllers;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,6 +10,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -21,11 +21,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.hcmute.endsemesterproject.R;
+import com.hcmute.endsemesterproject.Utils.CommonConst;
+
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
     private Button CreateAccountButton;
     private EditText UserEmail, UserPassword;
-    private TextView AlreadyHaveAccountLink;
+    private TextView AlreadyHaveAccountLink, txt_ErrorValidEmail;
     private ProgressDialog loadingBar;
     private FirebaseAuth mAuth;
     FirebaseDatabase database;
@@ -68,24 +71,19 @@ public class RegisterActivity extends AppCompatActivity {
         UserEmail = (EditText) findViewById(R.id.register_email);
         UserPassword = (EditText) findViewById(R.id.register_password);
         AlreadyHaveAccountLink = (TextView) findViewById(R.id.already_have_account_link);
+        txt_ErrorValidEmail = (TextView) findViewById(R.id.txt_errorValidMail);
+        txt_ErrorValidEmail.setVisibility(View.INVISIBLE);
         loadingBar = new ProgressDialog(this);
     }
 
     private void CreateNewAccount()
     {
-        String email = UserEmail.getText().toString();
-        String password = UserPassword.getText().toString();
+        String email = UserEmail.getText().toString().trim() ;
+        String password = UserPassword.getText().toString().trim();
+        txt_ErrorValidEmail.setVisibility(View.INVISIBLE);
 
-        if (TextUtils.isEmpty(email))
-        {
-            Toast.makeText(this, "Please enter email...", Toast.LENGTH_SHORT).show();
-        }
-        if (TextUtils.isEmpty(password))
-        {
-            Toast.makeText(this, "Please enter password...", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
+        if (isValidEmail(email) && CommonConst.isValidPassword(password, RegisterActivity.this)) {
+//            Toast.makeText(this, "Acc OKE", Toast.LENGTH_SHORT).show();
             loadingBar.setTitle("Creating New Account");
             loadingBar.setMessage("Please wait, while we wre creating new account for you...");
             loadingBar.setCanceledOnTouchOutside(true);
@@ -105,7 +103,7 @@ public class RegisterActivity extends AppCompatActivity {
                                 RootRef.child("Users").child(currentUserID).setValue("");
 
                                 RootRef.child("Users").child(currentUserID).child("device_token")
-                                       .setValue(deviceToken);
+                                        .setValue(deviceToken);
 
                                 SendUserToMainActivity();
                                 Toast.makeText(RegisterActivity.this, currentUserID, Toast.LENGTH_SHORT).show();
@@ -121,6 +119,27 @@ public class RegisterActivity extends AppCompatActivity {
                         }
                     });
         }
+    }
+
+    private boolean isValidEmail(String target) {
+        if (TextUtils.isEmpty(target)) {
+            Toast.makeText(this, "Email is incorrect !", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (!target.endsWith(CommonConst.DOMAIN_PART_GMAIL)) {
+            Toast.makeText(this, "Domain must: @gmail.com", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        String localPart = target.substring(0, target.length() - CommonConst.DOMAIN_PART_GMAIL.length());
+        String EMAIL_REGEX = "^[a-zA-Z0-9][a-zA-Z0-9.]*(\\.[a-zA-Z0-9]+)*[a-zA-Z0-9]$";
+        if(!Pattern.matches(EMAIL_REGEX, localPart)){
+            txt_ErrorValidEmail.setVisibility(View.VISIBLE);
+            return false;
+        }
+
+        return true;
     }
 
     private void SendUserToLoginActivity()
